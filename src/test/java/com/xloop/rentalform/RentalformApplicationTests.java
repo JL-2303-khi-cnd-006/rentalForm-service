4cff241af43ec763743dfa6693334da2bc9a9822
Javeria Lodhi <javeria.lodhi@xloopdigital.com>
2023-07-13 11:30:27 +0500

    initial commit

---
+++ src/test/java/com/xloop/rentalform/RentalformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xloop.rentalform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
